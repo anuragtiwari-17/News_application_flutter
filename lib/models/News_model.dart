c328571034fddd7ca0b8fbeb4713701937fb2f1c
anuragtiwari-17 <126300776+anuragtiwari-17@users.noreply.github.com>
2023-04-24 11:04:30 +0530

    Update News_model.dart

--- lib/models/News_model.dart
+++ lib/models/News_model.dart
@@ -9,7 +9,7 @@ class News_model {
     return News_model(
         title: map['title'],
         urlToImage: map['urlToImage'] ??
-            "https://images2.minutemediacdn.com/image/upload/c_fill,w_1440,ar_16:9,f_auto,q_auto,g_auto/shape/cover/sport/643188-gettyimages-153946385-ca1ccfaad9be44325afc434b305adc0d.jpg",
+            "https://images.app.goo.gl/J7HJ1pJHLJSJY8QG6",
         author: map['author'] ?? "Anomous");
   }
 }
